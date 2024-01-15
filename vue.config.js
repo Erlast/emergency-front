a8712c7bdbf4e4619354f91860d557475ee04421
Jane A <bunny9@bk.ru>
2024-01-15 21:21:35 +0300

    ПК (обновление, фильтры)

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  chainWebpack: config => {
-    config.module
-        .rule('images')
-        .set('parser', {
-          dataUrlCondition: {
-            maxSize: 4 * 1024 // 4KiB
-          }
-        })
-  },
-  // css: {
-  //   loaderOptions: {
-  //     css: {
-  //       modules: {
-  //         localIdentName: '[name]-[hash]',
-  //         exportLocalsConvention: 'camelCaseOnly'
-  //       }
-  //     }
-  //   }
-  // }
+    transpileDependencies: true,
+    chainWebpack: config => {
+        config.module
+            .rule('images')
+            .set('parser', {
+                dataUrlCondition: {
+                    maxSize: 4 * 1024 // 4KiB
+                }
+            })
+    },
+    // css: {
+    //   loaderOptions: {
+    //     css: {
+    //       modules: {
+    //         localIdentName: '[name]-[hash]',
+    //         exportLocalsConvention: 'camelCaseOnly'
+    //       }
+    //     }
+    //   }
+    // }
 })
